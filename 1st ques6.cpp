d6abba0bbc73d693ca1312c50cdca76e10340ac0
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-11-17 20:29:17 +0530

    Update 1st ques6.cpp

--- 1st ques6.cpp
+++ 1st ques6.cpp
@@ -7,5 +7,5 @@ cout << "Enter the radius of circle: ";
 cin >> r;
 cout << "Diameter" << 2 << "*" << r << "=" << 2*r;
 cout << " Circumference" << 44 << "*" << r << "/" << 7 << "=" << 44*r/7;
-cout << " Area" << 23 << "*" << r << "*" << r << "/" << 7 << "=" << 22*r*r/7;
+cout << " Area" << 22 << "*" << r << "*" << r << "/" << 7 << "=" << 22*r*r/7;
 }
